e14e6669c8f78b3b0340666cb19cddb4cf0e4f2f
Jung-ocean <j.jung@snu.ac.kr>
2021-07-07 11:51:44 +0900

    20210707 11:51:44

--- git/git_push.bash
+++ git/git_push.bash
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-repo='Source' # Remote repository name you want to pull
-reponame='origin' # Bookmark for the remote repository
+# You have to run git_pull.bash first
+# Changing or adding files should be done after git_pull.bash
+# Run git_push.bash after Changing or adding files 
+
+reponame='origin' # Bookmark for the remote repository. This should be the same as the name determined by git_pull.bash
 branch='main' # Branch name you want to push
 file2upload=$1 # File or direcotry you want to push
-
-#git init
-#git config --global user.name "Jung-ocean"
-#git config --global user.email j.jung@snu.ac.kr
-#git remote add ${reponame} https://github.com/Jung-ocean/${repo}.git
-#git remote set-url ${reponame} https://Jung-ocean@github.com/Jung-ocean/${repo}.git
-#git pull ${reponame} ${branch}
 
 git add ${file2upload}
 time=`date "+%Y%m%d %H:%M:%S"`
